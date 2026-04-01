15476760360940ad0c1d651c45989cb1b46f6078
XN <1214463068@qq.com>
2026-04-01 18:13:21 +0800

    增加战斗组件

---
+++ Source/Warrior/Private/AbilitySystem/Abilities/WarriorGameplayAbility.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
+#include "Components/Combat/PawnCombatComponent.h"
+void UWarriorGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilitySpec& Spec)
+{
+	Super::OnGiveAbility(ActorInfo, Spec);
+	
+	if (AbilityActivationPolicy==EwarriorAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo && !Spec.IsActive())
+		ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle);
+		
+	}
+}
+
+void UWarriorGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+	
+	if (AbilityActivationPolicy==EwarriorAbilityActivationPolicy::OnGiven)
+	{
+		if (ActorInfo )
+		{
+			ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+		}
+		
+	}
+	
+}
+
+UPawnCombatComponent* UWarriorGameplayAbility::GetCombatComponentFromActorInfo() const
+{
+	return GetAvatarActorFromActorInfo()->FindComponentByClass<UPawnCombatComponent>();
+	
+}
---
+++ Source/Warrior/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+//2026.3.30 创建一个基础的 GameplayAbility 类，添加一个枚举来控制技能的激活时机（OnTriggered 或 OnGiven）。在 OnGiveAbility 和 EndAbility 中根据激活策略自动尝试激活或清除技能。
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "WarriorGameplayAbility.generated.h"
+
+class UPawnCombatComponent;
+
+UENUM(BlueprintType)//2026.3.30
+enum class EwarriorAbilityActivationPolicy :uint8
+{
+	OnTriggered,
+	OnGiven
+};
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+protected:
+	//~Begin UGameplayAbility Interface
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	//~ End UGameplayAbility Interface
+	
+	UPROPERTY(EditDefaultsOnly,category = "WarriorAbility")
+	EwarriorAbilityActivationPolicy AbilityActivationPolicy= EwarriorAbilityActivationPolicy::OnTriggered;
+
+	UFUNCTION(BlueprintPure,Category="Ability")
+    UPawnCombatComponent* GetCombatComponentFromActorInfo() const;
+
+};
