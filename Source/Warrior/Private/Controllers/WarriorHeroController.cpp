8e5f8e8a2f5455576af131589ba4a047210b6f11
XN <1214463068@qq.com>
2026-05-18 17:09:29 +0800

    玩家控制器

--- Source/Warrior/Private/Controllers/WarriorHeroController.cpp
+++ Source/Warrior/Private/Controllers/WarriorHeroController.cpp
@@ -2,13 +2,27 @@
 
 
 #include "Controllers/WarriorHeroController.h"
+#include "Components/Team/TeamManagerComponent.h"
+#include "DataAssets/Team/DataAsset_TeamConfig.h"
 
 AWarriorHeroController::AWarriorHeroController()
 {
 	HeroTeamId = FGenericTeamId(0); // 设置默认玩家团队 ID，确保玩家角色被 AI 识别为友好目标。
+
+	TeamManagerComponent = CreateDefaultSubobject<UTeamManagerComponent>(TEXT("TeamManagerComponent"));
 }
 
 FGenericTeamId AWarriorHeroController::GetGenericTeamId() const
 {
 	return HeroTeamId;
+}
+
+void AWarriorHeroController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (TeamConfig && TeamManagerComponent)
+	{
+		TeamManagerComponent->InitializeTeam(TeamConfig);
+	}
 }
--- Source/Warrior/Public/Controllers/WarriorHeroController.h
+++ Source/Warrior/Public/Controllers/WarriorHeroController.h
@@ -7,8 +7,11 @@
 #include "GenericTeamAgentInterface.h"
 #include "WarriorHeroController.generated.h"
 
+class UTeamManagerComponent;
+class UDataAsset_TeamConfig;
+
 /**
- * 
+
  */
 UCLASS()
 class WARRIOR_API AWarriorHeroController : public APlayerController,public IGenericTeamAgentInterface
@@ -18,7 +21,16 @@ public:
 	AWarriorHeroController ();
 	//IGenericTeamAgentInterface接口函数
 	virtual FGenericTeamId GetGenericTeamId() const override;
-	
+
+protected:
+	virtual void BeginPlay() override;
+
 private:
 	FGenericTeamId HeroTeamId ; // 默认玩家团队 ID，确保玩家角色被 AI 识别为友好目标。
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Team", meta = (AllowPrivateAccess = "true"))
+	UTeamManagerComponent* TeamManagerComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Team", meta = (AllowPrivateAccess = "true"))
+	UDataAsset_TeamConfig* TeamConfig;
 };
