47eb4e990cac4dff1ccbb3d8db4a2805c9dd887c
XN <1214463068@qq.com>
2026-05-18 17:12:42 +0800

    存档功能

---
+++ Source/Warrior/Public/SaveGame/WarriorSaveGame.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "WarriorTypes/WarriorEnumTypes.h"
+#include "WarriorSaveGame.generated.h"
+
+
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UPROPERTY(BlueprintReadOnly, SaveGame, Category="SaveGame")
+	WarriorDifficulty SavedCurrentDifficult ;
+	
+	
+};
